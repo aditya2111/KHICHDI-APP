a157cb1961dde5a11267a10f1d4b9550bb48fd7d
imraghav20 <raghav20nov@gmail.com>
2020-01-17 22:12:59 +0530

    solved issue of images not displaying while using django by creating a static folder

--- Khichdi/urls.py
+++ Khichdi/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +26,5 @@ urlpatterns = [
     path('poll/', include('Poll.urls')),
     path('profile/', include('Profile.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
